9d80ee13958cb92e880875e0e74f83db5a386978
Andromeda Yelton <andromeda.yelton@gmail.com>
2014-10-25 11:19:59 -0400

    Update doc_page.js

--- js/doc_page.js
+++ js/doc_page.js
@@ -1,9 +1,11 @@
-$(document).ready(function () {
-  $('.top_link[data-toggle-selector]').on('click', function (event) {
-    var toToggle = $(event.target).data('toggle-selector');
-    var originallyVisible = $(toToggle).hasClass('visible');
-    $('.toc').removeClass('visible');
-    $(toToggle).toggleClass('visible', !originallyVisible);
+var $j = jQuery.noConflict();
+
+$j(document).ready(function () {
+  $j('.top_link[data-toggle-selector]').on('click', function (event) {
+    var toToggle = $j(event.target).data('toggle-selector');
+    var originallyVisible = $j(toToggle).hasClass('visible');
+    $j('.toc').removeClass('visible');
+    $j(toToggle).toggleClass('visible', !originallyVisible);
     return false;
   });
 });
